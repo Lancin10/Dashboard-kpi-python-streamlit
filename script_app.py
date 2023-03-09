09b49031f6aef655517e885b8ea28f621505b859
Lancine Sérifou <42044048+Lancin10@users.noreply.github.com>
2023-03-09 18:09:03 +0100

    Update script_app.py

--- script_app.py
+++ script_app.py
@@ -3,14 +3,14 @@
 
 
 
-import pandas as pd  # pip install pandas openpyxl
+import pandas as pd  # pour installer les library : pip install pandas openpyxl
 import plotly.express as px  # pip install plotly-express
 import streamlit as st  # pip install streamlit
 
 # emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
 st.set_page_config(page_title="Sales Dashboard", page_icon=":bar_chart:", layout="wide")
 
-# Function to read excel file
+# The Function to read excel file
 @st.cache
 def get_data_from_excel():
     df = pd.read_excel(
